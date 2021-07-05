fadff0e6dda6f665c71a8abae403f28bad2ee89a
Augusto Castro <augustocesarcastro026@gmail.com>
2021-07-04 23:14:35 -0300

    commented first image loader and added a second one, with a listener and two new functions(onLoadFailed and onResourceReady). Project finished

--- app/src/main/java/com/example/glideimageloader/MainActivity.kt
+++ app/src/main/java/com/example/glideimageloader/MainActivity.kt
@@ -1,9 +1,16 @@
 package com.example.glideimageloader
 
+import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
 import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import com.example.glideimageloader.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -17,12 +24,41 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+//        Glide.with(this)
+//            .load(imageLink)
+//            .fitCenter()
+//            .override(300,300)
+//            .diskCacheStrategy(DiskCacheStrategy.ALL)
+//            .placeholder(R.drawable.ic_launcher_background)
+//            .into(binding.image1)
+
         Glide.with(this)
             .load(imageLink)
-            .fitCenter()
-            .diskCacheStrategy(DiskCacheStrategy.ALL)
-            .placeholder(R.drawable.ic_launcher_background)
-            .into(binding.image1)
+            .listener(object : RequestListener<Drawable> {
+                override fun onLoadFailed(
+                    e: GlideException?,
+                    model: Any?,
+                    target: Target<Drawable>?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    Log.d("tag", "onLoadFailed: $$e")
+                    return false
+                }
+
+                override fun onResourceReady(
+                    resource: Drawable?,
+                    model: Any?,
+                    target: Target<Drawable>?,
+                    dataSource: DataSource?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    Toast.makeText(this@MainActivity,
+                        "Image ready",
+                        Toast.LENGTH_LONG).show()
+                    return false
+                }
+
+            }).into(binding.image1)
     }
 
 }
