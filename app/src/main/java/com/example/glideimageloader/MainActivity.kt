9c1ded20f7ad094902870e45362cf30e7fd7d72f
Augusto Castro <augustocesarcastro026@gmail.com>
2021-07-04 23:03:06 -0300

    added main activity layout and an image that can be loaded with Glide. Already tested

--- app/src/main/java/com/example/glideimageloader/MainActivity.kt
+++ app/src/main/java/com/example/glideimageloader/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.glideimageloader
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.example.glideimageloader.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private val imageLink = "https://upload.wikimedia.org/wikipedia/commons/4/47/Campbell_Island_Landscape.jpg"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        Glide.with(this)
+            .load(imageLink)
+            .fitCenter()
+            .diskCacheStrategy(DiskCacheStrategy.ALL)
+            .placeholder(R.drawable.ic_launcher_background)
+            .into(binding.image1)
     }
+
 }
